0eaf743316386639fa6db89799caf034608f1dbf
techie-mohit <mohitkeshari2000@gmail.com>
2025-08-06 12:33:28 +0530

    auth Controller

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -5,11 +5,11 @@ import * as authController from '../controllers/authController';
 const router = Router();
 
 router.post("/register", authController.register);
-router.get("/login", authController.login);
+router.post("/login", authController.login);
 router.get("/verify-email/:token", authController.verifyEmail);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password/:token", authController.resetPassword);
-router.post("/logout", authController.logout);
+router.get("/logout", authController.logout);
 
 
 export default router;
