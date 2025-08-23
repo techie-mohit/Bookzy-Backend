1ad72718d77816b6f4a902accd25800209e972ef
techie-mohit <mohitkeshari2000@gmail.com>
2025-08-23 19:59:44 +0530

    add routes in order

--- routes/orderRoutes.ts
+++ routes/orderRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import authenticateMidddleware from '../middleware/authMiddleware';
-import { createOrUpadateOrder, getOrderById, getOrderByUserId } from '../controllers/orderController';
+import { createOrUpadateOrder, getOrderById, getOrderByUserId, createPaymentWithRazorpay , handleRazorpayWebhook} from '../controllers/orderController';
 const router = express.Router();
 
 router.post('/', authenticateMidddleware, createOrUpadateOrder);
 router.get('/', authenticateMidddleware, getOrderByUserId);
 router.get('/:id', authenticateMidddleware, getOrderById);
+router.post('/payment-razorpay', authenticateMidddleware, createPaymentWithRazorpay);
+router.post('/razorpay-webhook', authenticateMidddleware, handleRazorpayWebhook);
 
 
 export default router;
